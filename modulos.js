006a5646aa63dc5866deecea8cf802913beb01db
FellipeCastro <fellipecastro27d@gmail.com>
2023-11-25 21:13:15 -0300

    Atualizações feitas!

--- modulos.js
+++ modulos.js
@@ -6,49 +6,49 @@ const listaModulos = [
     },
 
     {
-        "titulo": "Module 2: <br> Vocabulary and Expressions",
+        "titulo": "Module 02: <br> Vocabulary and Expressions",
         "img": "img/modulo-02.jpg",
         "descricao": "Building a strong vocabulary is crucial for effective communication. This module focuses on common vocabulary and everyday expressions, enabling you to navigate real-life situations with ease.",
     },
 
     {
-        "titulo": "Module 3: <br> Listening Comprehension",
+        "titulo": "Module 03: <br> Listening Comprehension",
         "img": "img/modulo-03.jpg",
         "descricao": "Listening is a key skill in language acquisition. You'll develop your ability to understand spoken English, with a focus on everyday conversations. Get ready for engaging listening practice that mirrors real-life situations.",
     },
 
     {
-        "titulo": "Module 4: <br> Basic Conversation",
+        "titulo": "Module 04: <br> Basic Conversation",
         "img": "img/modulo-04.jpg",
         "descricao": "Now that you've learned the essentials, it's time to put them into action. Module 4 teaches you to construct simple dialogues and provides plenty of opportunities for conversational practice.",
     },
 
     {
-        "titulo": "Module 5: <br> Intermediate Grammar",
+        "titulo": "Module 05: <br> Intermediate Grammar",
         "img": "img/modulo-05.jpg",
         "descricao": "Grammar can be a stumbling block, but not with our expert guidance. In this module, we delve into more advanced grammatical structures and help you master verb tenses, essential for expressing yourself accurately.",
     },
 
     {
-        "titulo": "Module 6: <br> Reading and Text Comprehension",
+        "titulo": "Module 06: <br> Reading and Text Comprehension",
         "img": "img/modulo-06.jpg",
         "descricao": "Reading is your gateway to a deeper understanding of the English language. You'll read authentic texts in English and work on comprehension skills, expanding your knowledge of vocabulary and context.",
     },
 
     {
-        "titulo": "Module 7: <br> Intermediate Writing",
+        "titulo": "Module 07: <br> Intermediate Writing",
         "img": "img/modulo-07.jpg",
         "descricao": "Writing is an art, and we're here to help you refine it. This module focuses on improving your writing skills and guides you in composing simple yet effective texts.",
     },
 
     {
-        "titulo": "Module 8: <br> Culture and Society of the World",
+        "titulo": "Module 08: <br> Culture and Society of the World",
         "img": "img/modulo-08.jpg",
         "descricao": "Dive into the rich and diverse culture of the English-speaking world in this module. Explore Anglo-Saxon culture, engage in discussions on cultural topics, and broaden your horizons.",
     },
 
     {
-        "titulo": "Module 9: Review and Advanced Preparation",
+        "titulo": "Module 09: <br> Review and Advanced Preparation",
         "img": "img/modulo-09.jpg",
         "descricao": "As you near the end of your journey, Module 10 offers a comprehensive review of the entire course. If you're up for the challenge, you can also prepare for an advanced-level course, taking your English proficiency to new heights.",
     },
